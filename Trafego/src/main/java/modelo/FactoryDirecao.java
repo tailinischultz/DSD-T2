a0e1812e4da036b5e8130f44ad688d8ceacca7e6
Lucas Pietro Biasi Rayzer <145388833+LucasRayzer@users.noreply.github.com>
2024-09-26 10:12:32 -0300

    update

--- Trafego/src/main/java/modelo/FactoryDirecao.java
+++ Trafego/src/main/java/modelo/FactoryDirecao.java
@@ -9,5 +9,47 @@ package modelo;
  * @author User
  */
 public class FactoryDirecao {
-    
+
+    public static Direcao getDirecao(int numDir) {
+        Direcao retorno = null;
+        switch(numDir) {
+            case 1:
+                retorno = Direcao.CIMA;
+                break;
+            case 2:
+                retorno = Direcao.DIREITA;
+                break;
+            case 3:
+                retorno = Direcao.BAIXO;
+                break;
+            case 4:
+                retorno = Direcao.ESQUERDA;
+                break;
+            case 5:
+                retorno = Direcao.CRUZAMENTO_CIMA;
+                break;
+            case 6:
+                retorno = Direcao.CRUZAMENTO_DIREITA;
+                break;
+            case 7:
+                retorno = Direcao.CRUZAMENTO_BAIXO;
+                break;
+            case 8:
+                retorno = Direcao.CRUZAMENTO_ESQUERDA;
+                break;
+            case 9:
+                retorno = Direcao.CRUZAMENTO_CIMA_DIREITA;
+                break;
+            case 10:
+                retorno = Direcao.CRUZAMENTO_CIMA_ESQUERDA;
+                break;
+            case 11:
+                retorno = Direcao.CRUZAMENTO_BAIXO_DIREITA;
+                break;
+            case 12:
+                retorno = Direcao.CRUZAMENTO_BAIXO_ESQUERDA;
+                break;
+        }
+        return retorno;
+    }
 }
