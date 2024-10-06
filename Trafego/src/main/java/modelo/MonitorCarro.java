294fb889801f8ae4f03970a43cd55ee854ff5c4c
Lucas Pietro Biasi Rayzer <145388833+LucasRayzer@users.noreply.github.com>
2024-10-06 17:43:31 -0300

    update

--- Trafego/src/main/java/modelo/MonitorCarro.java
+++ Trafego/src/main/java/modelo/MonitorCarro.java
@@ -1,5 +1,7 @@
 package modelo;
 
+import java.util.Random;
+
 public class MonitorCarro extends Carro {
 
     private String nomeCarro;
@@ -138,7 +140,186 @@ public class MonitorCarro extends Carro {
         }
         return proximoSegmento;
     }
-    
+    private Segmento getProximoSegmentoCruzamento(Segmento segmentoDeReferencia, boolean forcaSairCruzamento, Segmento segmentoRefAnterior, Segmento[] listaCaminho, int idxSegmentoAtual) {
+        Segmento proximoSegmento = null;
+        Segmento a = null;
+        Segmento b = null;
+        Random random = new Random();
+        boolean par = random.nextInt(500) % 2 == 0;
+
+        switch (segmentoDeReferencia.getDirecao()) {
+            case "Cruzamento_Cima":
+                if (forcaSairCruzamento) {
+                    proximoSegmento = this.getProximoSegmentoCruzamento(segmentoRefAnterior, true, null, null, 0);
+                    listaCaminho[idxSegmentoAtual] = null;
+                }
+                else {
+                    proximoSegmento = this.getProximoSegmentoCima(segmentoDeReferencia);
+                }
+                break;
+            case "Cruzamento_Direita":
+                if (forcaSairCruzamento) {
+                    proximoSegmento = this.getProximoSegmentoCruzamento(segmentoRefAnterior, true, null, null, 0);
+                    listaCaminho[idxSegmentoAtual] = null;
+                }
+                else {
+                    proximoSegmento = this.getProximoSegmentoDireita(segmentoDeReferencia);
+                }
+                break;
+            case "Cruzamento_Baixo":
+                if (forcaSairCruzamento) {
+                    proximoSegmento = this.getProximoSegmentoCruzamento(segmentoRefAnterior, true, null, null, 0);
+                    listaCaminho[idxSegmentoAtual] = null;
+                }
+                else {
+                    proximoSegmento = this.getProximoSegmentoBaixo(segmentoDeReferencia);
+                }
+                break;
+            case "Cruzamento_Esquerda":
+                if (forcaSairCruzamento) {
+                    proximoSegmento = this.getProximoSegmentoCruzamento(segmentoRefAnterior, true, null, null, 0);
+                    listaCaminho[idxSegmentoAtual] = null;
+                }
+                else {
+                    proximoSegmento = this.getProximoSegmentoEsquerda(segmentoDeReferencia);
+                }
+                break;
+            case "Cruzamento_baixo_Direita":
+                a = this.getProximoSegmentoBaixo(segmentoDeReferencia);
+                b = this.getProximoSegmentoDireita(segmentoDeReferencia);
+                if (forcaSairCruzamento) {
+                    proximoSegmento = a.isCruzamento() ? b : a;
+                }
+                else {
+                    proximoSegmento = par ? a : b;
+                }
+                break;
+            case "Cruzamento_Baixo_Esquerda":
+                a = this.getProximoSegmentoBaixo(segmentoDeReferencia);
+                b = this.getProximoSegmentoEsquerda(segmentoDeReferencia);
+                if (forcaSairCruzamento) {
+                    proximoSegmento = a.isCruzamento() ? b : a;
+                }
+                else {
+                    proximoSegmento = par ? a : b;
+                }
+                break;
+            case "Cruzamento_Cima_Direita":
+                a = this.getProximoSegmentoCima(segmentoDeReferencia);
+                b = this.getProximoSegmentoDireita(segmentoDeReferencia);
+                if (forcaSairCruzamento) {
+                    proximoSegmento = a.isCruzamento() ? b : a;
+                }
+                else {
+                    proximoSegmento = par ? a : b;
+                }
+                break;
+            case "Cruzamento_Cima_Esquerda":
+                a = this.getProximoSegmentoCima(segmentoDeReferencia);
+                b = this.getProximoSegmentoEsquerda(segmentoDeReferencia);
+                if (forcaSairCruzamento) {
+                    proximoSegmento = a.isCruzamento() ? b : a;
+                }
+                else {
+                    proximoSegmento = par ? a : b;
+                }
+                break;
+        }
+
+        return proximoSegmento;
+    }
+    private boolean temCaminhoDefinido() {
+        boolean encontrouCaminho = false;
+        for (Segmento segmento : caminhoACruzar) {
+            if (segmento != null) {
+                encontrouCaminho = true;
+            }
+        }
+        return encontrouCaminho;
+    }
+
+    private void escolherCaminhoCruzamento(Segmento segmentoIniCruzamento) {
+        this.caminhoACruzar[0] = segmentoIniCruzamento;
+        Segmento armazenaPassoAnterior = null;
+        for (int i = 1; i <= 3; i++) {
+            Segmento prox = this.getProximoSegmentoCruzamento(this.caminhoACruzar[(i - 1)], (i == 3), armazenaPassoAnterior, this.caminhoACruzar, (i - 1));
+            this.caminhoACruzar[i] = prox;
+            armazenaPassoAnterior = this.caminhoACruzar[(i - 1)];
+            if (!prox.isCruzamento()) {
+                break;
+            }
+        }
+
+        Segmento[] caminhoCruzamentoAux = new Segmento[4];
+        int contIdxAtual = 0;
+        for (Segmento segmento : caminhoACruzar) {
+            if (segmento != null) {
+                caminhoCruzamentoAux[contIdxAtual] = segmento;
+                contIdxAtual++;
+            }
+        }
+        this.caminhoACruzar = caminhoCruzamentoAux;
+    }
+
+    private synchronized void tentaReservarCaminhoCruzamento() {
+        boolean livre = true;
+        for (Segmento segmento : caminhoACruzar) {
+            if (segmento == null) {
+                break;
+            }
+            if (segmento.getReserva() != null) {
+                livre = false;
+                break;
+            }
+        }
+
+        if (!livre) {
+            return;
+        }
+
+        for (Segmento segmento :caminhoACruzar) {
+            if (segmento == null) {
+                break;
+            }
+            segmento.setReserva(this);
+        }
+    }
+
+    private boolean temCaminhoReservado() {
+        for (Segmento segmento : caminhoACruzar) {
+            if (segmento != null) {
+                if (segmento.getReserva() == this) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void andarNoCruzamento() {
+        for (int i = 0; i < this.caminhoACruzar.length; i++) {
+            Segmento segmento = this.caminhoACruzar[i];
+            if (segmento != null) {
+                this.andarUmSegmento(segmento);
+                segmento.setReserva(null);
+                this.caminhoACruzar[i] = null;
+                break;
+            }
+        }
+        boolean andouTudo = true;
+        for (int i = 0; i < this.caminhoACruzar.length; i++) {
+            if (this.caminhoACruzar[i] != null) {
+                andouTudo = false;
+                break;
+            }
+        }
+        if (andouTudo) {
+            this.emCruzamento = false;
+        }
+    }
     private void andarUmaCasaNormal(Segmento segmentoAIr) {
         if (segmentoAIr.isCruzamento()) {
             this.emCruzamento = true;
@@ -177,24 +358,24 @@ public class MonitorCarro extends Carro {
                 break;
             }
             if (this.emCruzamento) {
-                //if (this.temCaminhoDefinido()) {
-                //if (this.temCaminhoReservado()) {
-                //    this.trataAndarCruzamento();
-                //}
-                //else {
-                //    this.tentaReservarCaminhoCruzamento();
-                //    if (this.temCaminhoReservado()) {
-                //        this.trataAndarCruzamento();
-                //    }
-                // }
-                //}
-                // else {
-                //     this.escolherCaminhoCruzamento(proximoNodo);
-                //     this.tentaReservarCaminhoCruzamento();
-                //    if (this.temCaminhoReservado()) {
-                // this.trataAndarCruzamento();
-                // }
-                // }
+                if (this.temCaminhoDefinido()) {
+                if (this.temCaminhoReservado()) {
+                    this.andarNoCruzamento();
+                }
+                else {
+                    this.tentaReservarCaminhoCruzamento();
+                    if (this.temCaminhoReservado()) {
+                        this.andarNoCruzamento();
+                    }
+                 }
+                }
+                 else {
+                     this.escolherCaminhoCruzamento(proximoSegmento);
+                     this.tentaReservarCaminhoCruzamento();
+                    if (this.temCaminhoReservado()) {
+                 this.andarNoCruzamento();
+                 }
+                 }
             } else {
                 proximoSegmento = this.getProxNodoNormal();
                 this.andarUmaCasaNormal(proximoSegmento);
